98fc67fa57596005496010e58bd59d25940be1b0
Mani Shankar Kumar <manishankarkumar@Manis-MacBook-Air.local>
2020-12-12 01:33:13 +0530

    sf-movies-service

---
+++ src/main/java/com/sf/movies/SfMoviesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sf.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfMoviesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfMoviesServiceApplication.class, args);
+	}
+
+}
